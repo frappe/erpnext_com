a9782b58562d4042d6b3de5af2a2d6a9598ac061
Anand Doshi <anand@erpnext.com>
2016-04-28 19:25:14 +0530

    [fix] accept json for signup request and pipe validation messages

--- erpnext_com/api.py
+++ erpnext_com/api.py
@@ -1,20 +1,20 @@
 from __future__ import unicode_literals
+import requests, json
 import frappe
 from frappe import _
-import requests
 
 @frappe.whitelist(allow_guest=True)
 def signup(full_name, email, subdomain, plan="Free-Solo", distribution="erpnext"):
 	central_url = frappe.local.conf.central_url or 'https://central.erpnext.com'
 
 	# send request to central
-	r = requests.post(central_url.rstrip('/ ') + '/api/method/central.signup.signup', data={
+	r = requests.post(central_url.rstrip('/ ') + '/api/method/erpnext_central.signup.signup', data={
 		'full_name': full_name,
 		'email': email,
 		'subdomain': subdomain,
 		'plan': plan,
 		'distribution': distribution
-	})
+	}, headers={'Accept': 'application/json; q=1.0, */*; q=0.1'})
 
 	context = {
 		'pathname': 'schools/signup' if distribution=='schools' else 'signup'
@@ -22,12 +22,17 @@ def signup(full_name, email, subdomain, plan="Free-Solo", distribution="erpnext"
 
 	if r.status_code == 200:
 		location = frappe.redirect_to_message(_('Thank you for signing up'),
-			"""<div><p>You will receive an email at {},
+			"""<div><p>You will receive an email at <strong>{}</strong>,
 			asking you to verify this account request.<br>
 			If you are unable to find the email in your inbox, please check your SPAM folder.
 			It may take a few minutes before you receive this email.</p>
 			<p>Once you click on the verification link, your account will be ready in a few minutes.</p>
 			</div>""".format(email), context=context)
+
+	elif r.status_code in (417, 409):
+		# ValidationError, NameError
+		# show the message that we got from server
+		frappe.throw(json.loads(r.json()['_server_messages'])[0])
 
 	else:
 		# something went wrong
