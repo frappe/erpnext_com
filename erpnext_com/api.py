503ec3f92e58303ea03dc9030bb59cd9cfd7bd83
shreyas <shreyas@erpnext.com>
2016-06-21 12:57:40 +0530

    [Minor] Fixes for setting up central server erpnext.xyz

--- erpnext_com/api.py
+++ erpnext_com/api.py
@@ -2,25 +2,17 @@ from __future__ import unicode_literals
 import requests, json
 import frappe
 from frappe import _
+from central.signup import signup as _signup
 
 @frappe.whitelist(allow_guest=True)
 def signup(full_name, email, subdomain, plan="Free-Solo", distribution="erpnext"):
-	central_url = frappe.local.conf.central_url or 'https://central.erpnext.com'
-
-	# send request to central
-	r = requests.post(central_url.rstrip('/ ') + '/api/method/erpnext_central.signup.signup', data={
-		'full_name': full_name,
-		'email': email,
-		'subdomain': subdomain,
-		'plan': plan,
-		'distribution': distribution
-	}, headers={'Accept': 'application/json; q=1.0, */*; q=0.1'})
+	status = _signup(full_name, email, subdomain, distribution, plan)
 
 	context = {
 		'pathname': 'schools/signup' if distribution=='schools' else 'signup'
 	}
 
-	if r.status_code == 200:
+	if status == 'success':
 		location = frappe.redirect_to_message(_('Thank you for signing up'),
 			"""<div><p>You will receive an email at <strong>{}</strong>,
 			asking you to verify this account request.<br>
@@ -28,20 +20,18 @@ def signup(full_name, email, subdomain, plan="Free-Solo", distribution="erpnext"
 			It may take a few minutes before you receive this email.</p>
 			<p>Once you click on the verification link, your account will be ready in a few minutes.</p>
 			</div>""".format(email), context=context)
-
-	elif r.status_code in (417, 409):
-		# ValidationError, NameError
-		# show the message that we got from server
-		frappe.throw(json.loads(r.json()['_server_messages'])[0])
+	# TODO Need to decide whether this part of code is necessary or not
+	# elif r.status_code in (417, 409):
+	# 	# ValidationError, NameError
+	# 	# show the message that we got from server
+	# 	frappe.throw(json.loads(r.json()['_server_messages'])[0])
 
 	else:
 		# something went wrong
 		location = frappe.redirect_to_message(_('Something went wrong'),
 			'Please try again or drop an email to support@erpnext.com',
-			context=context, http_status_code=500)
+			context=context)
 
-	print location
 	return {
 		'location': location
 	}
-
