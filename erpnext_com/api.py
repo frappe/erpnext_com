44485e2bd0e65098ca09f1dfb1463b3dc41ea9e8
Rushabh Mehta <rmehta@gmail.com>
2017-02-13 15:03:10 +0530

    added tayseer as sponsor

--- erpnext_com/api.py
+++ erpnext_com/api.py
@@ -54,21 +54,16 @@ def make_payment(full_name, email, company, workshop=0, conf=0, currency='inr'):
 @frappe.whitelist(allow_guest=True)
 def signup(full_name, email, subdomain, plan="Free", distribution="erpnext", res=None):
 	status = _signup(full_name, email, subdomain, plan=plan,
-		distribution='erpnext' if distribution=='schools' else distribution,
-		reseller=res)
-
-	context = {
-		'pathname': 'schools/signup' if distribution=='schools' else 'signup'
-	}
+		distribution=distribution, reseller=res)
 
 	if status == 'success':
-		location = frappe.redirect_to_message(_('Thank you for signing up'),
+		location = frappe.redirect_to_message(_('Verify your Email'),
 			"""<div><p>You will receive an email at <strong>{}</strong>,
-			asking you to verify this account request.<br>
-			If you are unable to find the email in your inbox, please check your SPAM folder.
-			It may take a few minutes before you receive this email.</p>
-			<p>Once you click on the verification link, your account will be ready in a few minutes.</p>
-			</div>""".format(email), context=context)
+			asking you to verify this account request.<p>
+			<p>It may take a few minutes before you receive this email.
+			If you don't find it, please check your SPAM folder.</p>
+			<p>After verification, your account will be setup</p>
+			</div>""".format(email), indicator='blue')
 
 	elif status=='retry':
 		return {}
@@ -76,8 +71,7 @@ def signup(full_name, email, subdomain, plan="Free", distribution="erpnext", res
 	else:
 		# something went wrong
 		location = frappe.redirect_to_message(_('Something went wrong'),
-			'Please try again or drop an email to support@erpnext.com',
-			context=context)
+			'Please try again or drop an email to support@erpnext.com', indicator='red')
 
 	return {
 		'location': location
