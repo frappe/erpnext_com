f196b4499de0fbec1f4b54e30a897c5b35134a13
Saurabh <saurabh6790@gmail.com>
2017-09-01 18:34:39 +0530

    refactored pricing page UI

--- erpnext_com/www/pricing/index.py
+++ erpnext_com/www/pricing/index.py
@@ -1,14 +1,18 @@
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import cint
 
 def get_context(context):
-	plan_details = frappe.db.sql('''
+	plan_details = []
+	for plan in frappe.db.sql('''
 		select bp.name, bp.users, bp.space, bp.emails, bpa.amount
 				from `tabBase Plan` bp
 				left join `tabPlan Amount` bpa
 					on (bpa.parent = bp.name and bpa.currency="USD")
 				where bp.is_an_upgrade=1 order by bpa.amount asc
-	''', as_dict=1)
+	''', as_dict=1):
+		plan['amount'] = cint(plan['amount'])
+		plan_details.append(plan)
 
 	return {
 		"plan_details": plan_details
