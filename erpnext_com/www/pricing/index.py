dc81c298b01a30b290e6fbbf5ff15f600f1b79c7
Raghavendra Kamath <raghu@raghukamath.com>
2018-03-07 20:07:16 +0530

    Add new plans

--- erpnext_com/www/pricing/index.py
+++ erpnext_com/www/pricing/index.py
@@ -3,18 +3,51 @@ import frappe
 from frappe.utils import cint
 
 def get_context(context):
-	plan_details = []
-	for plan in frappe.db.sql('''
-		select bp.name, bp.users, bp.space, bp.emails, bpa.amount
-				from `tabBase Plan` bp
-				left join `tabPlan Amount` bpa
-					on (bpa.parent = bp.name and bpa.currency="USD")
-				where bp.is_an_upgrade=1 order by bpa.amount asc
-	''', as_dict=1):
-		plan['amount'] = cint(plan['amount'])
-		plan['space'] = cint(plan['space'])
-		plan_details.append(plan)
+	common_features = [
+		"Priority Support",
+		"Bug Fix Guarantee",
+		"All Modules",
+		"All Domains",
+		"Auto-Upgrades",
+		"Automated Backups",
+		"Custom Domain"
+	]
 
-	return {
-		"plan_details": plan_details
-	}
+	start_features = [
+		"10 Users",
+		"10 GB Space",
+		"10,000 Emails",
+	]
+
+	medium_features = [
+		"100 Users",
+		"100 GB Space",
+		"100,000 Emails",
+	]
+
+	enterprise_features = [
+		"Unlimited Users",
+		"Unlimited Space",
+		"Unlimited Emails",
+	]
+	enterprise_extras = [
+	]
+	plans = [
+		{"name": "Small",
+		"price": "$ 125<span class=\"small\">/month</span>",
+		"info": "Billed Annually",
+		"features": start_features + common_features
+		},
+
+		{"name": "Medium",
+		"price": "$ 800<span class=\"small\">/month</span>",
+		"info": "Billed Annually",
+		"features": medium_features + common_features
+		},
+		{"name": "Enterprise",
+		"price": "Custom",
+		"info": "Dedicated",
+		"features": enterprise_features + common_features
+		}
+	]
+	return {"plans": plans}
