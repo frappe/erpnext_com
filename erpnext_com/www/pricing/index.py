9f8b2cfe67308bd1eea47ea08086c44bebab9050
Faris Ansari <netchampfaris@users.noreply.github.com>
2018-03-08 20:24:52 +0530

    New Pricing Plans (#172)
    * Add new plans
    
    * Add more points in pricing
    
    * update compare page
    
    * remove uneeded comments
    
    * Change title and update feature list
    
    * Add contact page, convert button to link
    
    * Add contact.js

--- erpnext_com/www/pricing/index.py
+++ erpnext_com/www/pricing/index.py
@@ -3,18 +3,70 @@ import frappe
 from frappe.utils import cint
 
 def get_context(context):
-	plan_details = []
-	for plan in frappe.db.sql('''
-		select bp.name, bp.users, bp.space, bp.emails, bpa.amount
-				from `tabBase Plan` bp
-				left join `tabPlan Amount` bpa
-					on (bpa.parent = bp.name and bpa.currency="USD")
-				where bp.is_an_upgrade=1 order by bpa.amount asc
-	''', as_dict=1):
-		plan['amount'] = cint(plan['amount'])
-		plan['space'] = cint(plan['space'])
-		plan_details.append(plan)
+	common_features = [
+		"Priority Support",
+		"Bug Fix Guarantee",
+		"All Modules",
+		"All Domains",
+		"Auto-Upgrades",
+		"Automated Backups",
+		"Custom Domain",
+		"Mobile App"
+		]
 
-	return {
-		"plan_details": plan_details
-	}
+	start_features = [
+		"10 Users",
+		"10 GB Space",
+		"10,000 Emails",
+		]
+
+	medium_features = [
+		"100 Users",
+		"100 GB Space",
+		"100,000 Emails",
+		]
+
+	enterprise_features = [
+		"Unlimited Users",
+		"Unlimited Space",
+		"Unlimited Emails",
+		]
+
+	medium_extras = [
+		"5 Custom Print Formats",
+		"5 Custom Reports"
+		]
+
+	enterprise_extras = [
+		"Dedicated Engineer",
+		"Custom Print Formats",
+		"Custom Reports",
+		"Feature Development"
+		]
+
+	plans = [
+		{"name": "Small",
+		"price": "$ 125<span class=\"small\">/month</span>",
+		"info": "Billed Annually",
+		"cta": "Start Trial",
+		"ctalink": "href='/signup?plan=P10-2018'",
+		"features": start_features + common_features
+		},
+
+		{"name": "Medium",
+		"price": "$ 800<span class='small'>/month</span>",
+		"info": "Billed Annually",
+		"cta": "Start Trial",
+		"ctalink": "href='/signup?plan=P100-2018'",
+		"features": medium_features + common_features + medium_extras
+		},
+
+		{"name": "Enterprise",
+		"price": "Custom",
+		"info": "Dedicated Server",
+		"cta": "Contact Us",
+		"ctalink": "href='/contact'",
+		"features": enterprise_features + common_features + enterprise_extras
+		}
+		]
+	return {"plans": plans}
