29a97f560f3061a9daf2d95926b87cca2148f66b
Raghavendra Kamath <raghu@raghukamath.com>
2018-03-08 12:58:14 +0530

    Add more points in pricing

--- erpnext_com/www/pricing/index.py
+++ erpnext_com/www/pricing/index.py
@@ -11,43 +11,62 @@ def get_context(context):
 		"Auto-Upgrades",
 		"Automated Backups",
 		"Custom Domain"
-	]
+		]
 
 	start_features = [
 		"10 Users",
 		"10 GB Space",
 		"10,000 Emails",
-	]
+		]
 
 	medium_features = [
 		"100 Users",
 		"100 GB Space",
 		"100,000 Emails",
-	]
+		]
 
 	enterprise_features = [
 		"Unlimited Users",
 		"Unlimited Space",
 		"Unlimited Emails",
-	]
+		]
+
+	medium_extras = [
+		"5 Custom Print Formats",
+		"5 Custom Reports"
+		]
+
 	enterprise_extras = [
-	]
+		"Dedicated Engineer",
+		"Customisations",
+		"Custom Print Formats",
+		"Custom Reports",
+		"Feature Constibutions"
+		]
+
 	plans = [
 		{"name": "Small",
 		"price": "$ 125<span class=\"small\">/month</span>",
 		"info": "Billed Annually",
-		"features": start_features + common_features
+		"cta": "Start Trial",
+		"ctalink": "onclick=\"redirect_to_signup('signup', 'P10-2018')\"",
+		"features": start_features + common_features,
 		},
 
 		{"name": "Medium",
 		"price": "$ 800<span class=\"small\">/month</span>",
 		"info": "Billed Annually",
-		"features": medium_features + common_features
+		"cta": "Start Trial",
+		"ctalink":  "onclick=\"redirect_to_signup('signup', 'P100-2018')\"",
+		"features": medium_features + common_features + medium_extras
 		},
+
 		{"name": "Enterprise",
 		"price": "Custom",
-		"info": "Dedicated",
-		"features": enterprise_features + common_features
+		"info": "Dedicated Server",
+		"cta": "Contact Us",
+		"ctalink": "onclick=\"window.location='https://frappe.io/about#contact'\";",
+		"features": enterprise_features + common_features + enterprise_extras
 		}
-	]
+		]
 	return {"plans": plans}
