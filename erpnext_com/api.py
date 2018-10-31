c983de397b9073930acbe6ade5fc2e7e87ca128b
Saurabh <saurabh6790@gmail.com>
2018-10-31 11:48:08 +0530

    [fix] pass industry type at the time of signup

--- erpnext_com/api.py
+++ erpnext_com/api.py
@@ -57,8 +57,8 @@ def make_payment(full_name, email, company, workshop=0, conf=0, currency='inr'):
 	})
 
 @frappe.whitelist(allow_guest=True)
-def signup(full_name, email, subdomain, plan=None, distribution="erpnext", res=None, number_of_users=1):
-	resp = _signup(full_name, email, subdomain, plan=plan,
+def signup(full_name, email, subdomain, industry_type, plan=None, distribution="erpnext", res=None, number_of_users=1):
+	resp = _signup(full_name, email, subdomain, industry_type, plan=plan,
 		distribution=distribution, reseller=res, users=number_of_users)
 
 	if resp.get("redirect_to"):
