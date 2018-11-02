ecf1de7024dd54508b2cede9884132766d6bf979
Saurabh <saurabh6790@gmail.com>
2018-11-02 19:16:45 +0530

    poc_signle_step_to_desk

--- erpnext_com/api.py
+++ erpnext_com/api.py
@@ -57,9 +57,10 @@ def make_payment(full_name, email, company, workshop=0, conf=0, currency='inr'):
 	})
 
 @frappe.whitelist(allow_guest=True)
-def signup(full_name, email, subdomain, industry_type, plan=None, distribution="erpnext", res=None, number_of_users=1):
+def signup(full_name, email, subdomain, industry_type, plan=None, distribution="erpnext",
+	res=None, number_of_users=1, passphrase=None):
 	resp = _signup(full_name, email, subdomain, industry_type, plan=plan,
-		distribution=distribution, reseller=res, users=number_of_users)
+		distribution=distribution, reseller=res, users=number_of_users, password=passphrase)
 
 	if resp.get("redirect_to"):
 
