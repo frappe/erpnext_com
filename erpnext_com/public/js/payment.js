82547e2efacabe280ec6feea97d1f52af597c2d9
Prateeksha Singh <pratu16x7@gmail.com>
2018-11-16 15:28:20 +0530

    Add Disclaimer and Intercom

--- erpnext_com/public/js/payment.js
+++ erpnext_com/public/js/payment.js
@@ -22,3 +22,16 @@ window.pay = {
 		frappe.msgprint(msg, "Wire Transfer").addClass("wire-transfer-dialog");
 	},
 }
+
+
+var s = document.createElement( 'script' );
+s.textContent = `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/md8hmp1l';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()`;
+document.body.appendChild( s );
+
+window.Intercom("boot", {
+	app_id: "md8hmp1l"
+});
+
+frappe.route.on('change', () => {
+	window.Intercom("update");
+});
