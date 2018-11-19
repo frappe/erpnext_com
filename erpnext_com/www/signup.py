3b38ed5b5b204361830c6f68a7fa2287831c2696
Prateeksha Singh <pratu16x7@gmail.com>
2018-11-19 12:34:56 +0530

    remove payment div for trial

--- erpnext_com/www/signup.py
+++ erpnext_com/www/signup.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 import frappe
 from central.utils import get_signup_domain
+from central.signup import is_payment_mandatory
 no_cache = True
 
 def get_context(context):
 	return {
-		'signup_domain': get_signup_domain() or 'erpnext.com'
+		'signup_domain': get_signup_domain() or 'erpnext.com',
+		'is_payment_mandatory': is_payment_mandatory
 	}
