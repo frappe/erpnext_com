36b4ed7d82bf676ab99ce92e5067cf59aab3b2fd
Sahil Khan <sahil@erpnext.com>
2019-02-12 16:35:44 +0530

    fix(security): post references greater than 60 days.
    Co-authored-by: Chinmay Pai <chinmaydpai@gmail.com>
    Co-authored-by: Anurag Mishra <mishranaman123@gmail.com>

--- erpnext_com/www/security/references.py
+++ erpnext_com/www/security/references.py
@@ -5,5 +5,7 @@ import frappe
 no_cache = True
 
 def get_context(context):
-	references = frappe.get_all("CVE", fields="*")
+	references = frappe.get_all("CVE",
+		fields=["commit_url", "reference", "reporter_url", "reported_by", "affected_applications", "severity"],
+		filters=[['reported_on', '>', frappe.utils.add_days(frappe.utils.now_date(), -60)]])
 	return {"references": references}
