75a5cbe8e289605f6c84b94e40f7186f0e66631b
Chinmay Pai <chinmaydpai@gmail.com>
2019-02-13 14:29:14 +0530

    fix(references): reported_date -> reported_on

--- erpnext_com/www/security/references.py
+++ erpnext_com/www/security/references.py
@@ -7,5 +7,5 @@ no_cache = True
 def get_context(context):
 	references = frappe.get_all("CVE",
 				    fields=["commit_url", "reference", "reporter_url", "reported_by", "affected_applications", "severity"],
-				    filters=[['reported_date', '<', frappe.utils.add_months(frappe.utils.nowdate(), -2)]])
+				    filters=[['reported_on', '<', frappe.utils.add_months(frappe.utils.nowdate(), -2)]])
 	return {"references": references}
