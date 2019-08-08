5bf38f50b8ffcf2fa6bc2a4ebb32c38b5e0083ea
Shivam Mishra <scm.mymail@gmail.com>
2019-08-08 20:28:55 +0530

    chore: added frappe landing page to website_generators

--- erpnext_com/hooks.py
+++ erpnext_com/hooks.py
@@ -82,7 +82,7 @@ home_page = "index"
 # ----------
 
 # automatically create page for each record of this doctype
-# website_generators = ["Web Page"]
+website_generators = ["Frappe Landing Page"]
 
 # Installation
 # ------------
