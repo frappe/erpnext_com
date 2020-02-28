688074cad37a06f70ee8a15b4b7b6cbaa7b4d3dc
Suraj Shetty <surajshetty3416@gmail.com>
2020-02-28 20:57:25 +0530

    feat: Add config

--- .travis/spell_checker.sh
+++ .travis/spell_checker.sh
@@ -2,4 +2,4 @@
 
 files=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
 echo $files
-exec npx cspell --no-summary -- $files
+exec npx cspell --config ./spell_checker/cSpell.json --no-summary -- $files
